203bde6507f68b5894f1b8b91c69a5f8a7e6c0e3
poyen0806 <leo33917888@gmail.com>
2024-11-04 15:54:18 +0800

    adjust: - remove data path since them are not frequently used - change k-value to half of features

--- config.py
+++ config.py
@@ -1,8 +1,4 @@
-# data path
-TRAIN_DATA_PATH = 'data/train.csv'
-TEST_DATA_PATH = 'data/test.csv'
-
 # parameters
 RANDOM_STATE = 42
 TEST_SIZE = 0.2
-FEATURE_SELECTION_K = 10
+FEATURE_SELECTION_K = 17
